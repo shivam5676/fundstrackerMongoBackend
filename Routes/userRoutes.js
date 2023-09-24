430bd615b590e831d97e4c693df653721af684bf
shivam5676 <techking08@gmail.com>
2023-09-24 00:56:16 -0700

    login route added with user email and password authentication if both is present then userwill get msg succeeful otherwise unsuccessful

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const signupTable = require("../models/signup");
 const router = express.Router();
+router.post("/login", async(req, res, next) => {
+  const data = req.body;
+  try{
+     const loginResponse=await signupTable.findAll({
+    where:{
+      email:data.email
+    } 
+  })
+  if(loginResponse.length===0){
+    return res.status(404).json({msg:"user does not exist with this email id"})
+  }
+ const passwordResponse=await signupTable.findAll({
+  where:{
+    password:data.password
+  }
+ })
+ if(passwordResponse.length===0){
+  return res.status(401).json({msg:"password is not match"})
+}
+
+return res.status(201).json({msg:"user login successfully"})
+  }
+  catch(err){
+    console.log(err)
+  }
+ 
+});
 
 router.post("/signup", async (req, res, next) => {
   const data = req.body;
@@ -17,10 +44,10 @@ router.post("/signup", async (req, res, next) => {
         password: data.password,
         email: data.email,
       });
-      console.log(signupResponse);
+
       return res.status(200).json({ msg: "account successfully created" });
     } else {
-      return res.status(200).json({
+      return res.status(401).json({
         msg: "An account with the same email id is present. Please try with another email id.",
       });
     }
