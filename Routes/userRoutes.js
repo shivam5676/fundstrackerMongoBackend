62a0380d90e2fd3e31ce4bd805e2a9956ee9f5e3
shivam5676 <techking08@gmail.com>
2023-09-24 20:14:07 -0700

    login and signup component now using bcrypt hashing and salting technique for saving password and comparing in database

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,37 +1,47 @@
 const express = require("express");
 const signupTable = require("../models/signup");
 const router = express.Router();
-router.post("/login", async(req, res, next) => {
+const bcrypt = require("bcrypt");
+router.post("/login", async (req, res, next) => {
   const data = req.body;
-  try{
-     const loginResponse=await signupTable.findAll({
-    where:{
-      email:data.email
-    } 
-  })
-  if(loginResponse.length===0){
-    return res.status(404).json({msg:"user does not exist with this email id"})
-  }
- const passwordResponse=await signupTable.findAll({
-  where:{
-    password:data.password
-  }
- })
- if(passwordResponse.length===0){
-  return res.status(401).json({msg:"password is not match"})
-}
+  try {
+    const savedUser = await signupTable.findOne({
+      where: {
+        email: data.email,
+      },
+    });
 
-return res.status(201).json({msg:"user login successfully"})
+    if (savedUser.length === 0) {
+      return res
+        .status(404)
+        .json({ msg: "user does not exist with this email id" });
+    }
+    try {
+      const comparedHashPassword = await bcrypt.compare(
+        data.password,
+        savedUser.password
+      );
+      console.log("compare", comparedHashPassword);
+      if (comparedHashPassword === true) {
+        console.log("execute");
+        return res.status(200).json({ msg: "Account successfully loggined" });
+      }
+      else {
+        return res.status(401).json({ msg: "Incorrect password" });
+      }
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ msg: "Error during password verification" });
+    }
+  } catch (err) {
+    res.status(500).json({ msg: "Internal server error" });
   }
-  catch(err){
-    console.log(err)
-  }
- 
 });
 
 router.post("/signup", async (req, res, next) => {
   const data = req.body;
-  //unique account validation by email
+  // Unique account validation by email
   try {
     const response = await signupTable.findAll({
       where: {
@@ -39,20 +49,36 @@ router.post("/signup", async (req, res, next) => {
       },
     });
     if (response.length === 0) {
-      const signupResponse = await signupTable.create({
-        name: data.name,
-        password: data.password,
-        email: data.email,
-      });
+      const saltrounds = 10;
 
-      return res.status(200).json({ msg: "account successfully created" });
+      try {
+        //we using a different try catch block only for getting error related to hasing process
+        // Attempt to hash the password
+        const encryptedPassword = await bcrypt.hash(data.password, saltrounds);
+
+        // If hashing is successful, create the user
+        const signupResponse = await signupTable.create({
+          name: data.name,
+          password: encryptedPassword,
+          email: data.email,
+        });
+
+        return res.status(200).json({ msg: "Account successfully created" });
+      } catch (hashingError) {
+        console.error("Error during password hashing:", hashingError);
+        return res
+          .status(500)
+          .json({ msg: "Error creating the account. Please try again later." });
+      }
     } else {
       return res.status(401).json({
         msg: "An account with the same email id is present. Please try with another email id.",
       });
     }
   } catch (err) {
-    console.log(err);
+    console.error("Error during account validation:", err);
+    res.status(500).json({ msg: "Internal server error" });
   }
 });
+
 module.exports = router;
