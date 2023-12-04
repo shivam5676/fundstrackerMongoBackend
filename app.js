cd0a80667f2d74abf9059e690c2a8a18230a5d62
shivam singh <ssultanpura9@gmail.com>
2023-12-03 22:41:15 -0800

    some cors issue fixed

--- app.js
+++ app.js
@@ -1,60 +1,61 @@
 const express = require("express");
 const userRoutes = require("./Routes/userRoutes");
-const premiumUserRoutes=require("./Routes/premiumUser")
+const premiumUserRoutes = require("./Routes/premiumUser");
 const sequelize = require("./util/database");
-const download=require("./models/downloadUrl")
+const download = require("./models/downloadUrl");
 const users = require("./models/user");
 const Expenses = require("./models/expense");
 const bodyParser = require("body-parser");
 const Order = require("./models/order");
 const user = require("./models/user");
-const https=require("https")
-const dotenv=require("dotenv")
-const forgotPasswordRequest=require("./models/forgotPassword")
+const https = require("https");
+const dotenv = require("dotenv");
+const forgotPasswordRequest = require("./models/forgotPassword");
 const cors = require("cors");
 const fs = require("fs");
 
-
-dotenv.config()
+dotenv.config();
 // const privatekey=fs.readFileSync("server.key")
 // const certificate=fs.readFileSync("server.cert")
 
 users.hasMany(Expenses);
 Expenses.belongsTo(users);
 
-users.hasMany(Order)
-Order.belongsTo(user)
+users.hasMany(Order);
+Order.belongsTo(user);
 
 users.hasMany(forgotPasswordRequest);
-forgotPasswordRequest.belongsTo(users)
+forgotPasswordRequest.belongsTo(users);
 
-users.hasMany(download)
-download.belongsTo(users)
+users.hasMany(download);
+download.belongsTo(users);
 
 const app = express();
 
 app.use(bodyParser.json({ extended: false }));
 
-app.use(cors({
-  origin: '*'
-}))
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    methods: process.env.METHODS,
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
+);
 
 app.use("/user", userRoutes);
-app.use("/premiumUser",premiumUserRoutes)
+app.use("/premiumUser", premiumUserRoutes);
 sequelize
   .sync()
   .then((result) => {
-   
-console.log(result)
+    console.log(result);
     app.listen(process.env.PORT);
-  //  now we will not use this we will use https server
+    //  now we will not use this we will use https server
 
-  //  https.createServer(app).listen(8000,()=>{
-  //   console.log("app running")
-  //  })
- 
+    //  https.createServer(app).listen(8000,()=>{
+    //   console.log("app running")
+    //  })
   })
   .catch((err) => {
     console.log(err);
-
   });
